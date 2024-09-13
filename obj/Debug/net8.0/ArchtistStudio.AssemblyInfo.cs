cad5b2914a089ead54bb52dc9c5c9092664cae82
SengVichet <Sengvichet2503@gmail.com>
2024-09-13 09:22:22 +0700

    delete

--- obj/Debug/net8.0/ArchtistStudio.AssemblyInfo.cs
+++ obj/Debug/net8.0/ArchtistStudio.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ArchtistStudio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0aad77a229732a303d22d449866014d39e33fe4b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00fa2a010e883fe0df513a610b7623776dc4d0f5")]
 [assembly: System.Reflection.AssemblyProductAttribute("ArchtistStudio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ArchtistStudio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
