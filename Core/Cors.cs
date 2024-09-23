70c91a386a5aa48afc9050c525677c13d2ecf678
SengVichet <Sengvichet2503@gmail.com>
2024-09-23 10:07:16 +0700

    back

--- Core/Cors.cs
+++ Core/Cors.cs
@@ -5,15 +5,14 @@ internal static class CorsExtension
     public static void AddMyCors(this IServiceCollection service)
     {
         service.AddCors(options =>
-     {
-         options.AddPolicy("AllowFlutterApp",
-             builder =>
-             {
-                 builder.WithOrigins("https://architecture.ts-enc.com")  
+        {
+            options.AddDefaultPolicy(
+                policy =>
+                    policy.WithOrigins("*")
+                        .AllowAnyMethod()
                         .AllowAnyHeader()
-                        .AllowAnyMethod();
-             });
-     });
-
+                        .SetIsOriginAllowedToAllowWildcardSubdomains()
+            );
+        });
     }
 }
